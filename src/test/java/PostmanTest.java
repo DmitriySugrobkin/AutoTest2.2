e006154fff095acdb56bcf7255ea9e9e7171300e
Sugrobkin <dsugrobkin148@gmail.com>
2023-06-16 19:08:13 +0300

    Add Test with bag

--- src/test/java/PostmanTest.java
+++ src/test/java/PostmanTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class PostmanTest {
     @Test
@@ -16,10 +17,9 @@ public class PostmanTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body(/* --> ваша проверка здесь <-- */)
+                .body("data", equalTo("some value"))
         ;
 
     }
 }
 
-}
